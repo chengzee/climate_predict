c56182ec53e9a3cc2a529ddf712e2871a59a65f0
chengzee <as0780962@gmail.com>
2021-01-20 20:09:25 +0800

    update results of input_seq length = 504

--- orchid_ClimatePredict/luongAtt_byCZ.py
+++ orchid_ClimatePredict/luongAtt_byCZ.py
@@ -7,11 +7,11 @@ import tensorflow as tf
 import os
 # from tensorflow.compat.v1.keras.layers import CuDNNLSTM
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 physical_devices = tf.config.list_physical_devices('GPU')
 try:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    tf.config.experimental.set_memory_growth(physical_devices[1], True)
 except:
     # Invalid device or cannot modify virtual devices once initialized.
     pass
@@ -38,7 +38,7 @@ count = 1
 the_first_nonzero = 0
 the_last_nonzero = 0
 n = 0
-increase_length = 144
+increase_length = 216
 _lookback = 288+increase_length
 _delay = 12*6
 sample_list = []
@@ -152,7 +152,7 @@ class Decoder(tf.keras.layers.Layer):
         
         return output_seq
 
-for A in range(3, A_layers):
+for A in range(A_layers):
     for neuron in neurons:
         # if neuron == 64 and (A == 64 or A == 128):
         #     BATCH_SIZE = 2048
@@ -161,6 +161,8 @@ for A in range(3, A_layers):
         if neuron == 1024:
             BATCH_SIZE = 128
             _epochs = 50
+            if A >= 3:
+                BATCH_SIZE = 64
         total_loss = np.zeros((_epochs))
         total_val_loss = np.zeros((_epochs))
         total_test_mse = 0
